482de2fc97d7565195bd2d02a0ba0d5c5916080a
Leone <leo.vernisse@gmail.com>
2025-01-02 15:29:53 +0100

    chore: manual upgrade to tauri v2 (#466)
    * chore: manual upgrade to tauri v2
    
    * fix: workflows

---
+++ src-next/out/_next/static/chunks/pages/_app-87129beae4dcfa26.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{1496:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(8329)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(1496),n(3675))),_N_E=_.O()}]);
---
+++ src-next/out/_next/static/chunks/pages/_error-2418c93dbdfadd8e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{8414:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(1976)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(8414)),_N_E=_.O()}]);
